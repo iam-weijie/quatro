6cb0784731abc76ceb9aaa2a7821416c8fd9686e
Weijie Wang <wang.weijie0110@gmail.com>
2025-06-17 14:16:25 -0400

    board game -> chess game

--- app/layout.js
+++ app/layout.js
@@ -13,7 +13,7 @@ const geistMono = Geist_Mono({
 
 export const metadata = {
   title: "Quatro",
-  description: "Board game with 4 diff pieces",
+  description: "Chess game with 4 diff pieces",
 };
 
 export default function RootLayout({ children }) {
